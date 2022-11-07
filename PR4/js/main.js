90560c74f421fea0ebfd179800d8921ea6840fa0
ShostakAndrii <95017755+ShostakAndrii@users.noreply.github.com>
2022-11-07 21:46:54 +0200

    PR4.3

--- PR4/js/main.js
+++ PR4/js/main.js
@@ -1,20 +1,10 @@
-// Modal
+"use strict";
+
+new WOW().init();
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-cartButton.addEventListener("click", toggleModal);
-
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open")
-}
-
-// Initialize WOW.js
-new WOW().init();
-
-// Auth
 const buttonAuth = document.querySelector(".button-auth");
 const modalAuth = document.querySelector(".modal-auth");
 const closeAuth = document.querySelector(".close-auth");
@@ -22,8 +12,18 @@ const logInForm = document.querySelector("#logInForm");
 const loginInput = document.querySelector("#login");
 const userName = document.querySelector(".user-name");
 const buttonOut = document.querySelector(".button-out");
+const cardsRestaurants = document.querySelector(".cards-restaurants");
+const containerPromo = document.querySelector(".container-promo");
+const restaurants = document.querySelector(".restaurants");
+const menu = document.querySelector(".menu");
+const logo = document.querySelector(".logo");
+const cardsMenu = document.querySelector(".cards-menu");
 
 let login = localStorage.getItem("login");
+
+function toggleModal() {
+  modal.classList.toggle("is-open")
+}
 
 function toggleModalAuth() {
   modalAuth.classList.toggle("is-open");
@@ -97,8 +97,6 @@ function notAuthorized() {
   })
 }
 
-buttonAuth.addEventListener("click", clearForm);
-
 function checkAuth() {
   if (login){
     authorized();
@@ -107,4 +105,87 @@ function checkAuth() {
   }
 }
 
+function createCardRestaurant() {
+  const card = `
+    <a class="card card-restaurant wow animate__animated animate__pulse" data-wow-delay="0.2s">
+      <img src="img/pizza-plus/preview.jpg" alt="Image" class="card-image">
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title">Пицца плюс</h3>
+          <span class="card-tag tag">50 мин</span>
+        </div>
+        <div class="card-info">
+          <div class="rating">
+            <img src="img/icon/rating.svg" class="rating-star">
+            4.5
+          </div>
+          <div class="price">От 900 ₽</div>
+          <div class="category">Пицца</div>
+        </div>
+      </div>
+    </a>
+  `;
+
+  cardsRestaurants.insertAdjacentHTML('beforeend', card);
+
+}
+
+function createCardGood() {
+  const card = document.createElement("div");
+  card.className = "card wow animate__animated animate__pulse";
+
+  card.insertAdjacentHTML("beforeend", `
+    <img src="img/food-band/pepperoni.jpg" alt="Image" class="card-image">
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title card-title-reg">Пепперони</h3>
+      </div>
+      <div class="card-info">
+        <div class="ingredients">Рис, угорь, соус унаги, кунжут, водоросли нори.</div>
+      </div>
+      <div class="card-buttons">
+        <button class="button button-primary">
+          <span class="button-card-text">В корзину</span>
+          <img src="img/icon/shopping-cart-white.svg" alt="Cart" class="button-card-image">
+        </button>
+        <strong class="card-price-bold">250 ₽</strong>
+      </div>
+    </div>
+  `);
+
+  cardsMenu.insertAdjacentElement("beforeend", card)
+}
+
+function openGoods(event) {
+  const target = event.target;
+
+  const restaurant = target.closest(".card-restaurant");
+
+  if (restaurant) {
+    containerPromo.classList.add("hide");
+    restaurants.classList.add("hide");
+    menu.classList.remove("hide");
+
+    cardsMenu.textContent = "";
+
+    createCardGood();
+    createCardGood();
+    createCardGood();
+  }
+}
+
+cardsRestaurants.addEventListener("click", openGoods);
+logo.addEventListener("click", function() {
+  containerPromo.classList.remove("hide");
+  restaurants.classList.remove("hide");
+  menu.classList.add("hide");
+})
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+buttonAuth.addEventListener("click", clearForm);
+
 checkAuth();
+
+createCardRestaurant();
