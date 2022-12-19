a5af2524418f6df9f6c146c5a63167bb85caec8c
ShostakAndrii <95017755+ShostakAndrii@users.noreply.github.com>
2022-12-19 20:34:09 +0200

    PR6.5

--- PR6/main.js
+++ PR6/main.js
@@ -1,130 +1,148 @@
-const $btn = document.getElementById("btn-kick");
-const $btn1 = document.getElementById("btn-kick1");
-const character = {
+import Pokemon from "./pokemon.js";
+import random from "./utils.js";
+
+const player1 = new Pokemon({
   name: "Pikachu",
   type: "electric",
-  weakness: ["fighting", "water", "some"],
-  resistance: ["steel"],
-  hp: {
-    current: 100,
-    total: 100,
-  },
-  damageHP: 100,
-  elHP: document.getElementById("health-character"),
-  elProgressbar: document.getElementById("progressbar-character"),
-  renderHP: renderHP,
-  changeHP: changeHP,
-  renderHPLife: renderHPLife,
-  renderProgressbarHP: renderProgressbarHP,
-}
+  hp: 500,
+  selectors: "character",
+});
 
-const enemy = {
+const player2 = new Pokemon({
   name: "Charmander",
-  type: "electric",
-  weakness: ["fighting", "water", "some"],
-  resistance: ["steel"],
-  hp: {
-    current: 100,
-    total: 100,
-  },
-  damageHP: 100,
-  elHP: document.getElementById("health-enemy"),
-  elProgressbar: document.getElementById("progressbar-enemy"),
-  renderHP: renderHP,
-  changeHP: changeHP,
-  changeHP1: changeHP1,
-  renderHPLife: renderHPLife,
-  renderProgressbarHP: renderProgressbarHP,
-}
+  type: "fire",
+  hp: 450,
+  selectors: "enemy",
+});
+
+
+
+const $btn = document.getElementById("btn-kick");
+const $btn1 = document.getElementById("btn-kick1");
+
+const clickCount = 6;
+// const character = {
+//   name: "Pikachu",
+//   type: "electric",
+//   weakness: ["fighting", "water", "some"],
+//   resistance: ["steel"],
+//   hp: {
+//     current: 100,
+//     total: 100,
+//   },
+//   damageHP: 100,
+//   elHP: document.getElementById("health-character"),
+//   elProgressbar: document.getElementById("progressbar-character"),
+//   renderHP: renderHP,
+//   changeHP: changeHP,
+//   renderHPLife: renderHPLife,
+//   renderProgressbarHP: renderProgressbarHP,
+// }
+//
+// const enemy = {
+//   name: "Charmander",
+//   type: "electric",
+//   weakness: ["fighting", "water", "some"],
+//   resistance: ["steel"],
+//   hp: {
+//     current: 100,
+//     total: 100,
+//   },
+//   damageHP: 100,
+//   elHP: document.getElementById("health-enemy"),
+//   elProgressbar: document.getElementById("progressbar-enemy"),
+//   renderHP: renderHP,
+//   changeHP: changeHP,
+//   changeHP1: changeHP1,
+//   renderHPLife: renderHPLife,
+//   renderProgressbarHP: renderProgressbarHP,
+// }
 
 const init = () => {
-  let clickCount = 6;
-
   $btn.innerText = `Thunder Jolt (${clickCount-1})`;
   $btn1.innerText = `Thunder Jolt (${clickCount-1})`;
 
-  const clickCounter1 = clickCounterFunc(clickCount);
-  const clickCounter2 = clickCounterFunc(clickCount);
-
   $btn.addEventListener("click", function() {
-    clicksLeft1 = clickCounter1();
-    if (clicksLeft1 > 0) {
-      console.log("Kick");
-      character.changeHP(random(20));
-      enemy.changeHP(random(20));
+    if (clickCounter1() > 0) {
+      player1.changeHP(random(60, 20), function(count) {
+        console.log("Some change after change HP", count);
+        console.log(generateLog(player1, player2, count));
+      });
+      player2.changeHP(random(60, 20));
+      // character.changeHP(random(20));
+      // enemy.changeHP(random(20));
       $btn.innerText = `Thunder Jolt (${clicksLeft1-1})`;
     }
   });
 
   $btn1.addEventListener("click", function() {
-    clicksLeft2 = clickCounter2();
-    if (clicksLeft2 > 0) {
-      console.log("Kick");
-      enemy.changeHP1(random(20));
+    if (clickCounter2() > 0) {
+      player2.changeHP(random(60, 20));
+      // enemy.changeHP1(random(20));
       $btn1.innerText = `Thunder Jolt (${clicksLeft2-1})`;
     }
   });
 
   console.log("Start Game!");
 
-  character.renderHP(character);
-  enemy.renderHP(enemy);
+  // player1.renderHP(player1);
+  // player2.renderHP(player2);
 }
 
-function renderHP() {
-  this.renderHPLife();
-  this.renderProgressbarHP();
-}
-
-function renderHPLife() {
-  this.elHP.innerText = this.hp.current + " / " + this.hp.total;
-}
-
-function renderProgressbarHP() {
-  this.elProgressbar.style.width = this.hp.current + "%";
-}
-
-function changeHP(count) {
-  this.hp.current -= count;
-
-  // console.log(generateLog(this));
-  const log = this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count);
-  // console.log(log);
-
-  if (this.hp.current <= 0) {
-    this.hp.current = 0;
-    alert("Бедный " + this.name + " проиграл бой!");
-    $btn.disabled = true;
-    $btn1.disabled = true;
-  }
-
-  // if (this.damageHP < count) {
-  //   this.damageHP = 0;
-  //   alert("Бедный " + this.name + " проиграл бой!");
-  //   $btn.disabled = true;
-  // } else {
-  //   this.damageHP -= count;
-  // }
-
-  this.renderHP();
-}
-
-function changeHP1(count) {
-  this.hp.current -= count;
-
-  if (this.hp.current <= 0) {
-    this.hp.current = 0;
-    alert("Бедный " + this.name + " проиграл бой!");
-    $btn.disabled = true;
-    $btn1.disabled = true;
-  }
-
-  this.renderHP();
-}
-
-const random = (num) => {
-  return Math.ceil(Math.random() * num);
-};
+// function renderHP() {
+//   this.renderHPLife();
+//   this.renderProgressbarHP();
+// }
+//
+// function renderHPLife() {
+//   this.elHP.innerText = this.hp.current + " / " + this.hp.total;
+// }
+//
+// function renderProgressbarHP() {
+//   this.elProgressbar.style.width = this.hp.current + "%";
+// }
+//
+// function changeHP(count) {
+//   this.hp.current -= count;
+//
+//   // console.log(generateLog(this));
+//   const log = this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count);
+//   // console.log(log);
+//
+//   if (this.hp.current <= 0) {
+//     this.hp.current = 0;
+//     alert("Бедный " + this.name + " проиграл бой!");
+//     $btn.disabled = true;
+//     $btn1.disabled = true;
+//   }
+//
+//   // if (this.damageHP < count) {
+//   //   this.damageHP = 0;
+//   //   alert("Бедный " + this.name + " проиграл бой!");
+//   //   $btn.disabled = true;
+//   // } else {
+//   //   this.damageHP -= count;
+//   // }
+//
+//   this.renderHP();
+// }
+//
+// function changeHP1(count) {
+//   this.hp.current -= count;
+//
+//   if (this.hp.current <= 0) {
+//     this.hp.current = 0;
+//     alert("Бедный " + this.name + " проиграл бой!");
+//     $btn.disabled = true;
+//     $btn1.disabled = true;
+//   }
+//
+//   this.renderHP();
+// }
+//
+// const random = (num) => {
+//   return Math.ceil(Math.random() * num);
+// };
 
 const generateLog = (firstPerson, secondPerson, count) => {
   const logs = [
@@ -149,5 +167,8 @@ const clickCounterFunc = (leftClicks) => {
     return leftClicks;
   };
 };
+
+const clickCounter1 = clickCounterFunc(clickCount);
+const clickCounter2 = clickCounterFunc(clickCount);
 
 init();
